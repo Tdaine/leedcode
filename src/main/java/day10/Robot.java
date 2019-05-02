ee928a749970ca85ef02720f76a2b6519dcd02cb
Tdaine <1916686509@qq.com>
2019-05-03 00:21:06 +0800

    '2019/5/3_two'

--- src/main/java/day10/Robot.java
+++ src/main/java/day10/Robot.java
@@ -13,7 +13,34 @@ public class Robot {
             return 1;
         return countWay(x - 1,y) + countWay(x, y - 1);
     }
+
+    /**
+     * 动态规划求法
+     * 这里将最右列和最下边一列全部初始化为1，因为只有一种走法
+     * 而dp[x][y]的走法就等于dp[x+1][y]+dp[x][y+1]
+     * @param x
+     * @param y
+     * @return
+     */
+    public static int countWay2(int x, int y){
+        int[][] dp = new int[x][y];
+        for (int i = 0; i < x; i++){
+            dp[i][y - 1] = 1;
+        }
+        for(int j = 0; j < y; j++){
+            dp[x - 1][j] = 1;
+        }
+
+        for (int i = x - 2; i >= 0; i--){
+            for (int j = y - 2; j >= 0; j--){
+                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+            }
+        }
+        return dp[0][0];
+    }
+
     public static void main(String[] args) {
-        System.out.println(countWay(2,2));
+        System.out.println(countWay2(3,9));
     }
 }
+
