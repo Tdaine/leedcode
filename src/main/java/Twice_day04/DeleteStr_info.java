5de5da912e1d2a47c15ea3b94c81f3525de34220
Tdaine <1916686509@qq.com>
2019-05-25 17:46:06 +0800

    '2019/5/25'

--- src/main/java/Twice_day04/DeleteStr_info.java
+++ src/main/java/Twice_day04/DeleteStr_info.java
@@ -1,4 +1,15 @@
 package Twice_day04;
 
-public class DeleteStr_info {
-}
+/**
+ * 输入两个字符串，从第一字符串中删除第二个字符串中所有的字符。例如，输入”They are
+ * students.”和”aeiou”，则删除之后的第一个字符串变成”Thy r stdnts.”
+ * 输入描述：
+ * 每个测试输入包含2个字符串
+ * 输出描述：
+ * 输出删除后的字符串
+ * 示例1:
+ * 输入
+ * They are students. aeiou
+ * 输出
+ * Thy r stdnts.
+ */
