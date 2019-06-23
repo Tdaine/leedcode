c52e0b8245f32362d9a51f01ccff5551f96903ca
Tdaine <1916686509@qq.com>
2019-06-23 21:28:23 +0800

    2019/6/23

--- src/main/java/Twice_day06/HalfInteger.java
+++ src/main/java/Twice_day06/HalfInteger.java
@@ -21,4 +21,29 @@ public class HalfInteger {
         }
         System.out.println(flag - '0');
     }
+
+    public static void solution(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()){
+            String str = in.nextLine();
+            String[] strs = str.split(" ");
+            int[] arr = new int[strs.length];
+            for (int i = 0; i < arr.length; i++){
+                arr[i] = Integer.valueOf(strs[i]);
+            }
+            int num = arr[0];
+            int count = 0;
+            for (int j = 1; j < arr.length; j++){
+                if (arr[j] == num){
+                    count++;
+                }else if (count > 0){
+                    count--;
+                }else {
+                    num = arr[j];
+                }
+            }
+            System.out.println(num);
+        }
+    }
 }
