46fb416566d59d90ebbe2cf5ae08893c3b6f5291
Tdaine <1916686509@qq.com>
2019-06-25 10:56:38 +0800

    删除 2019/6/25

--- src/main/java/Twice_day08/DeleteArr.java
+++ src/main/java/Twice_day08/DeleteArr.java
@@ -1,5 +1,7 @@
 package Twice_day08;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class DeleteArr {
@@ -27,6 +29,35 @@ public class DeleteArr {
                     }else count++;
                 }
             }
+        }
+    }
+
+    /**方法2：
+     * 使用ArrayList集合人的位置，因为报数为3就退出也就是每隔两个就会退出
+     * 设当前坐标为i,则i+ 2就是下一个要退出人的坐标，又因为是环形退出，所以需要将坐标进行循环
+     * 这里就需要(i + 2) % list.size()这样求出的坐标就是在list.size()中循环，
+     * 直到list.size() == 1 也就是只剩下一个人的时候退出循环，
+     */
+    public static void solution2( ) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()){
+            int n = in.nextInt();
+
+            if (n > 1000){
+                n = 999;
+            }
+
+            List<Integer> list = new ArrayList<Integer>();
+            for (int i = 0; i < n; i++){
+                list.add(i);
+            }
+
+            int i = 0;
+            while (list.size() > 1){
+                i = (i + 2) % list.size();
+                list.remove(i);
+            }
+            System.out.println(list.get(0));
         }
     }
 }
