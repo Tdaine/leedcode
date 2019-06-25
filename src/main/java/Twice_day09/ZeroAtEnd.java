702261c0284e72e86e4134d9fc83ef858ec68da6
Tdaine <1916686509@qq.com>
2019-06-25 17:24:48 +0800

    末尾0的个数 2019/6/25

--- src/main/java/Twice_day09/ZeroAtEnd.java
+++ src/main/java/Twice_day09/ZeroAtEnd.java
@@ -3,24 +3,26 @@ package Twice_day09;
 import java.util.Scanner;
 
 public class ZeroAtEnd {
+    /**
+     * 解决思路是，因为尾数为0的数都是一个偶数和5的乘积，所以如果想知道这个数阶乘的尾数有多少个0
+     * 可以将这个数进行拆分，求每个数分别包含多少个5，这样成在一起就有多少个0（比如说5，就是分别求5 4 3 2 1），
+     * 在求解的过程中会有隐含的5，比如151可以拆分成(150 149 ... 2 1)而其中的150有多少个5呢？
+     * 150%5==0，那是一个5吗？不是，因为150*偶数可以有两个0，这是因为里面有隐含的5，150/5=30，30%5==0，30/5==6；
+     * 所以在求解的过程中需要找出所有的隐含5，也就是循环除5，直到不是5的倍数，这个数字才算找到了所有的5.之后再加上其他分支中5的个数就是结果了。
+     * 当然也可以用求包含的偶数个数，但是因为偶数个数太多了所以就求5的个数
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        while (in.hasNext()){
-            int n = in.nextInt();
-            if (n < 1 && n > 1000)
-                return;
-            double multiple = 1;
-            for (int i = n; i > 0; i--){
-                multiple *= i;
-            }
-            System.out.println(multiple);
-            int count = 0;
-            int i = 10;
-            while (multiple % i == 0){
+        int n = in.nextInt();
+        int count = 0;
+        for (int i = n; i >= 5; i--){
+            int temp = i;
+             while (temp % 5 == 0){
+                temp = temp / 5;
                 count++;
-                i *= 10;
             }
-            System.out.println(count);
         }
+        System.out.println(count);
     }
 }
---
+++ src/main/java/choice/day09/test.java
@@ -0,0 +1,16 @@
+package choice.day09;
+
+public class test {
+    private static void testMethod(){
+        System.out.println("testMethod");
+    }
+    public static void main(String[] args) {
+//        String x = "fmn";
+//        x.toUpperCase();
+//        System.out.println(x);
+//        String y = x.replace('f','F');
+//        y = y + "wxy";
+//        System.out.println(y);
+        ((test)null).testMethod();
+    }
+}
