9bf64f77fece08664c9cd3cb359786fa2b41686e
Tdaine <1916686509@qq.com>
2019-06-25 17:44:37 +0800

    回文解码 2019/6/25

--- src/main/java/Twice_day09/PalindromeDecoding.java
+++ src/main/java/Twice_day09/PalindromeDecoding.java
@@ -5,13 +5,21 @@ import java.util.Scanner;
 public class PalindromeDecoding {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        StringBuffer sb = new StringBuffer(in.nextLine());
+        String str = in.nextLine();
+
         int n = in.nextInt();
         while (n-- > 0){
             int left = in.nextInt();
             int right = left + in.nextInt();
-            sb.append(new StringBuffer(sb.toString().substring(left,right)).reverse());
+            String start = str.substring(0,right);
+            String mid = str.substring(left,right);
+            String end = str.substring(right,str.length());
+            StringBuilder sb = new StringBuilder();
+            sb.append(start);
+            sb.append(new StringBuilder(mid).reverse());
+            sb.append(end);
+            str = sb.toString();
         }
-        System.out.println(sb);
+        System.out.println(str);
     }
 }
