6b944a864523390280612424753e25f1f2248d51
Tdaine <1916686509@qq.com>
2019-06-29 15:28:37 +0800

    个位数统计

--- src/main/java/Twice_day11/StatisticsUnit.java
+++ src/main/java/Twice_day11/StatisticsUnit.java
@@ -1,8 +1,6 @@
 package Twice_day11;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class StatisticsUnit {
     public static void main(String[] args) {
@@ -23,4 +21,25 @@ public class StatisticsUnit {
             }
         }
     }
+
+    public static void solution2(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String N = in.next();
+        char[] chars = N.toCharArray();
+
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++){
+            list.add(0);
+        }
+
+        for (int i = 0; i < 10; i++){
+            list.set(chars[i] - '0',list.get(chars[i] - '0') + 1);
+        }
+
+        for (int i = 0; i < list.size(); i++){
+            if (list.get(i) > 0)
+                System.out.println(i + ":" + list.get(i));
+        }
+    }
+
 }
