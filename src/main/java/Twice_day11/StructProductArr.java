1368b3fd962715cbf939f784e98305881da93725
Tdaine <1916686509@qq.com>
2019-06-29 16:03:52 +0800

    构建乘积数组

--- src/main/java/Twice_day11/StructProductArr.java
+++ src/main/java/Twice_day11/StructProductArr.java
@@ -1,44 +1,22 @@
 package Twice_day11;
-import java.util.ArrayList;
-import java.util.List;
 
-
-class KeyValue{
-    public int key;
-    public int value;
-
-    public KeyValue(int key, int value) {
-        this.key = key;
-        this.value = value;
-    }
-}
 
 class Solution {
     public int[] multiply(int[] A) {
-        List<KeyValue> list = new ArrayList<KeyValue>();
-        for (int i = 0; i < A.length;i++){
-            list.add(new KeyValue(i,1));
-        }
-        for (int i = 0; i < A.length; i++){
-            for (int j = 0; j < A.length;j++){
-                if (i != j){
-                    list.add(new KeyValue(j,list.get(j).value + 1));
-                }
-            }
-        }
-        int[] ret = new int[list.size()];
-        for (int i = 0; i < list.size(); i++){
-            ret[i] = list.get(i).value;
-            System.out.print(ret[i] + " ");
-        }
-        return ret;
+       int length = A.length;
+       int[] B = new int[length];
+
+       B[0] = 1;
+       for (int i = 1; i < length; i++){
+           B[i] = B[i - 1] * A[i - 1];
+       }
+
+       int temp = 1;
+       for (int i = length - 2; i >= 0; i--){
+           temp *= A[i + 1];
+           B[i] *= temp;
+       }
+       return B;
     }
 }
 
-public class StructProductArr {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] arr = new int[]{1,2,3,4,5};
-        solution.multiply(arr);
-    }
-}
---
+++ src/main/java/Twice_day11/StructProductArr_info.java
@@ -0,0 +1,13 @@
+package Twice_day11;
+
+/**
+ * @author abaka
+ * @date 2019/6/29 16:02
+ */
+
+/**
+ * 链接：https://www.nowcoder.com/questionTerminal/94a4d381a68b47b7a8bed86f2975db46
+ * 来源：牛客网
+ *
+ * 给定一个数组A[0,1,...,n-1],请构建一个数组B[0,1,...,n-1],其中B中的元素B[i]=A[0]*A[1]*...*A[i-1]*A[i+1]*...*A[n-1]。不能使用除法。
+ */
