df018ddcb0795ceee99b4b7675ec29d5d57ff408
Tdaine <1916686509@qq.com>
2019-07-06 17:15:51 +0800

    密码检验

--- src/main/java/Twice_day28/CheckPassword.java
+++ src/main/java/Twice_day28/CheckPassword.java
@@ -7,6 +7,11 @@ import java.util.Scanner;
  * @date 2019/7/6 16:47
  */
 public class CheckPassword {
+    /**
+     * 解决方法就是按照题目中的条件一个一个进行验证，验证方法也是比较基础的方法
+     * 这道题不要觉得太麻烦，虽然写起来很啰嗦但是很好理解
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         while (in.hasNext()){
@@ -18,6 +23,7 @@ public class CheckPassword {
         }
     }
 
+    //检验长度
     private static boolean checkLength(String str){
         if (str.length() > 8)
             return true;
@@ -25,6 +31,7 @@ public class CheckPassword {
             return false;
     }
 
+    //检验种类
     private static boolean checkKinds(String str){
         int digit = 0,lowercase = 0,uppercase = 0,others = 0;
         char[] arr = str.toCharArray();
@@ -48,6 +55,7 @@ public class CheckPassword {
         return n >= 3 ? true:false;
     }
 
+    //检验是否重复
     private static boolean checkRepeat(String str){
         for (int i = 0; i < str.length() - 2; i++){
             String repeat = str.substring(i,i + 3);
