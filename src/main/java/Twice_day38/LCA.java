1ed2ae4ec2ea9212705c90b6735f73492c3c705e
Tdaine <1916686509@qq.com>
2019-07-19 15:26:14 +0800

    最近公共祖先

--- src/main/java/Twice_day38/LCA.java
+++ src/main/java/Twice_day38/LCA.java
@@ -7,21 +7,21 @@ package Twice_day38;
 public class LCA {
     public int getLCA(int a, int b) {
         // write code here
-        if (a > b){
-            if (a / 2 == b)
-                return b;
-            else
-                return getLCA(a / 2,b);
-        }else {
-            if (a == b/2)
-                return a;
-            else return getLCA(a,b/2);
-        }
+//        if (a > b){
+//            if (a / 2 == b)
+//                return b;
+//            else
+//                return getLCA(a / 2,b);
+//        }else {
+//            if (a == b/2)
+//                return a;
+//            else return getLCA(a,b/2);
+//        }
 
-//        if (a == b)
-//            return a;
-//        if (a > b)
-//            return getLCA(a / 2,b);
-//        else return getLCA(a,b/2);
+        if (a == b)
+            return a;
+        if (a > b)
+            return getLCA(a / 2,b);
+        else return getLCA(a,b/2);
     }
 }
