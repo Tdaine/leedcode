260b05237bdaa553639e1915438cdf335158b71a
Tdaine <1916686509@qq.com>
2019-08-08 11:48:56 +0800

    2019/8/8

--- src/main/java/Twice_day15/UnusuasAdd.java
+++ src/main/java/Twice_day15/UnusuasAdd.java
@@ -1,14 +1,26 @@
 package Twice_day15;
 
 class UnusualAdd {
+    /**
+     * 二进制加法特点
+     * 位的异或运算跟求‘和’的结果一致：
+     *  异或 1^1 = 0   1^0 = 1 0^0 = 0
+     *  求和 1+1 = 0   1+0 = 1 0+0 = 0
+     * 位的与运算跟求‘进位’的结果一致:
+     *  位 与 1&1 = 1 1&0 = 0 0&0 = 0
+     *  进位  1+1 = 1 1+0 = 1 0+0 = 0
+     * @param A
+     * @param B
+     * @return
+     */
     public int addAB(int A, int B) {
         // write code here
         int xor,and;
         while (B != 0){
-            xor = A^B;
-            and = (A&B) << 1;
+            xor = A^B;//求和，但是不加进位
+            and = (A&B) << 1;//求进位
             A = xor;
-            B = and;
+            B = and;//如果进位为0，那么当前的和(A)就是最后的结果
         }
         return A;
     }
